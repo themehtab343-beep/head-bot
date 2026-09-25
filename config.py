03e010c519b9f52111fff40772110b3283ab939e
Mehtab Sandhdhu <themehtab343@gmail.com>
2026-09-25 21:44:17 +0530

    Initialize config.py with environment variables
    Add configuration settings for API and database.

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "36845944"))
+API_HASH = os.environ.get("API_HASH", "52a5e3343ba1edfe88ca570b42d15e7d")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8414448580:AAHR3GBt2OPoZllc4uZaL9kf7lnhBXqonqA")
+OWNER_ID = int(os.environ.get("OWNER_ID", "8796084661"))
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://admin:RbT1OMvw1wd0Z5vZ@cluster0.km6ffva.mongodb.net/?appName=Cluster0")
